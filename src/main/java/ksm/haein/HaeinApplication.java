054450597de732d39edad23c38025fd5ca26d0f8
noturavrigk <ksm92038386@gmail.com>
2025-07-05 17:13:25 +0900

    Initial commit: Spring 프로젝트 업로드

---
+++ src/main/java/ksm/haein/HaeinApplication.java
@@ -0,0 +1,13 @@
+package ksm.haein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaeinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaeinApplication.class, args);
+	}
+
+}
