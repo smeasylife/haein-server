f2fca6024f1a8290e2f42d796c9dbeb79a116457
noturavrigk <ksm92038386@gmail.com>
2025-07-07 16:00:33 +0900

    Feat: add member entity

---
+++ src/main/java/ksm/haein/user/entity/Member.java
@@ -0,0 +1,27 @@
+package ksm.haein.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+}
